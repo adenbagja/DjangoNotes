1d68aea16643243de808e5e474130658f1f2c258
adenbagja <adenbagja46@gmail.com>
2022-04-08 06:38:04 +0700

    add login base.html

--- notes/forms.py
+++ notes/forms.py
@@ -7,7 +7,7 @@ from .models import Notes
 class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
-        fields = {'title', 'text'}
+        fields = ('title', 'text')
 
     def clean_title(self):
         title = self.cleaned_data['title']
